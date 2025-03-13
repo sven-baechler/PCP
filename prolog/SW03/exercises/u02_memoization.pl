6c23a09520155ee081c093539b6f17434a9c3cf5
Sven Bächler <sven.baechler@stud.hslu.ch>
2025-03-13 17:25:22 +0100

    kommentar erweitert

--- prolog/SW03/exercises/u02_memoization.pl
+++ prolog/SW03/exercises/u02_memoization.pl
@@ -5,7 +5,7 @@ fak(0, 1). % Basisfall: Fakultät von 0 ist 1 (per Definition)
 fak(N, F) :-
     fak_as(N, F), % überprüfen, ob der Wert bereits in fak_as/2 gespeichert wurde
     format('(Hinweis: Fakulät von ~w war gespeichert.)', [N]), % Ausgabe
-    !. % ein Green Cut verhindert, dass weitere Ergebnisse angezeigt werden
+    !. % ein Green Cut verhindert, dass weitere Ergebnisse gespeichert werden
 fak(N, F) :-
     N > 0, % sicherstellen, dass N grösser 0 ist
     N1 is N - 1, % neues N1 ist N - 1
